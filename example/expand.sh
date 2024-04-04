b49663515a50641eeda230efafff3e013404b2f8
WumaCoder <1829913225@qq.com>
2024-04-04 23:09:23 +0800

    chore: add debug

---
+++ example/expand.sh
@@ -0,0 +1,6 @@
+echo "#![feature(print_internals)]" > ../example-expand/src/main.rs
+echo "#![feature(panic_internals)]" >> ../example-expand/src/main.rs
+echo "#![feature(alloc)]" >> ../example-expand/src/main.rs
+echo "#![feature(fmt_helpers_for_derive)]" >> ../example-expand/src/main.rs
+echo "#![allow(warnings, unused)]" >> ../example-expand/src/main.rs
+cargo expand >> ../example-expand/src/main.rs
