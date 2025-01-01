54879f77ef8bf2bec1a8f5e2baf11ba074c54ac5
Lydanne <1829913225@qq.com>
2025-01-01 17:40:21 +0800

    fix: comment out disable_auto_json in API macro

--- packages/nidrs-openapi-macro/src/lib.rs
+++ packages/nidrs-openapi-macro/src/lib.rs
@@ -27,7 +27,7 @@ pub fn api(args: TokenStream, input: TokenStream) -> TokenStream {
     }
 
     quote! {
-        #[meta(disable_auto_json = true)]
+        // #[meta(disable_auto_json = true)]
         #[meta(
             nidrs::openapi::RouterIn(
                 nidrs::openapi::RouterParams::default()
